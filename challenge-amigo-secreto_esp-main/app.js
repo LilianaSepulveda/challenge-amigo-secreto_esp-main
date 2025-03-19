74ca1ca6204c6b4bf33f3112de2b54c009895067
LilianaSepulveda <lilianasepulveda13@gmail.com>
2025-03-18 19:16:05 -0500

    update

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,30 +2,30 @@
 let listNames = [];
 
 //Adicionar amigos
-function agregarAmigo() {
-    let nombreAmigo = document.getElementById("amigo").value;
-    if (nombreAmigo == "") {
-        alert("Por favor, ingresa un nombre válido");
+    function agregarAmigo() {
+        let nombreAmigo = document.getElementById("amigo").value;
+        if (nombreAmigo == "") {
+            alert("Por favor, ingresa un nombre válido");
 
-    } else {
-        listNames.push(nombreAmigo);
-        document.getElementById("amigo").value = "";
-        listaActualizada();
+        } else {
+            listNames.push(nombreAmigo);
+            document.getElementById("amigo").value = "";
+            listaActualizada();
 
+        }
     }
-}
     //actualizar lista de amigos
-function listaActualizada() {
+    function listaActualizada() {
         let listaActualizada = document.getElementById("listaAmigos");
         listaActualizada.innerHTML = "";
         for (let i = 0; i < listNames.length; i++) {
             let itemLista = document.createElement("li");
             itemLista.innerHTML = listNames[i];
             listaActualizada.appendChild(itemLista);
+            }
         }
-    }
     //nombre aleatorio
-function sortearAmigo() {
+    function sortearAmigo() {
             if (listNames.length == 0) {
                 alert("Por favor, ingresar un nombre.");
                 return;
@@ -35,12 +35,5 @@ function sortearAmigo() {
             
             document.getElementById("resultado").innerHTML = `El amigo secreto sorteado es: ${nombreAleatorio}`;
         }
-
-
-
-
-
-
-
 
 
